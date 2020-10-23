28d4800cb77cb93ca74af26162901497ee65c7ab
RoyTien <chanjing910514@gmail.com>
2020-10-23 19:47:38 +0800

    t

--- server/session.go
+++ server/session.go
@@ -595,6 +595,7 @@ func (s *Session) leave(msg *ClientComMessage) {
 			s.queueOut(ErrPermissionDeniedReply(msg, msg.Timestamp))
 		} else {
 			// Unlink from topic, topic will send a reply.
+			log.Printf("LEAVE | %s | %s | %s \n", msg.Leave.Id, msg.AsUser, msg.RcptTo)
 			s.delSub(msg.RcptTo)
 			s.inflightReqs.Add(1)
 			sub.done <- &sessionLeave{
